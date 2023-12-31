b8ff49cbcfb5ff8710218b026846bead15f8a3ac
dannyhma <dannyhimawan089@gmail.com>
2023-12-31 14:04:33 +0700

    arrow

---
+++ public/arrow.js
@@ -0,0 +1,28 @@
+// function perkalian(x, y) {
+//   return x * y;
+// }
+
+// const hasil = perkalian(5, 9);
+// console.log(hasil);
+
+// const hasil = function (x, y) {
+//   return x * y;
+// };
+
+// console.log(hasil(12, 13));
+
+// const perkalian = (x, y) => {
+//   return x * y;
+// };
+
+// console.log(perkalian(10, 11));
+
+// const random = () => {
+//   return Math.floor(Math.random() * 10) + 1;
+// };
+
+// console.log(random());
+
+const random = () => Math.floor(Math.random() * 10) + 1;
+
+console.log(random());
