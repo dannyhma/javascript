aba51485905ad1da21e021fbb1e8482d8e600abe
dannyhma <dannyhimawan089@gmail.com>
2024-01-01 12:57:54 +0700

    merge object

--- public/spread.js
+++ public/spread.js
@@ -1,22 +1,31 @@
-const favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
-const others = ['Cake', 'Pie', 'Donut'];
+// const favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
+// const others = ['Cake', 'Pie', 'Donut'];
 
-const allFavorites = [...favorites, ...others];
-console.log(allFavorites);
+// const allFavorites = [...favorites, ...others];
+// console.log(allFavorites);
 
-const obj1 = { firstName: 'Obi-Wan', age: 30 };
-const obj2 = { lastName: 'Kenobi', gender: 'M' };
+// const obj1 = { firstName: 'Obi-Wan', age: 30 };
+// const obj2 = { lastName: 'Kenobi', gender: 'M' };
 
-const newObj = { ...obj1, ...obj2 };
-console.log(newObj);
+// const newObj = { ...obj1, ...obj2 };
+// console.log(newObj);
 
-const angka = [1, 2, 3, 4, 5];
+// const angka = [1, 2, 3, 4, 5];
 
-const max = Math.max(...angka);
-console.log(max);
+// const max = Math.max(...angka);
+// console.log(max);
 
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const name = ['Alexander', 'John', 'Doe', 'Thomas', 'George', 'Smith', 'Jeff'];
 
 const numName = [...name, ...number];
 console.log(numName);
+
+const user = {
+  name: 'John',
+  email: 'john@example.com',
+};
+console.log(user);
+
+const newUser = { ...user, id: 123, password: 'password' };
+console.log(newUser);
