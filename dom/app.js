e5b5c1470825d54af783a6ffc1a03604e3dc73ce
dannyhma <dannyhimawan089@gmail.com>
2024-01-02 22:27:08 +0700

    classlist

--- dom/app.js
+++ dom/app.js
@@ -1,5 +1,14 @@
-const headings = document.querySelectorAll("h1");
+const h1 = document.querySelector('h1');
 
-for (let heading of headings) {
-  heading.style.color = "blue";
-}
+// for (let heading of headings) {
+//   heading.style.color = "blue";
+// }
+
+h1.getAttribute('class');
+h1.setAttribute('class', 'bg-info');
+
+let currentClass = h1.getAttribute('class');
+h1.setAttribute('class', `${currentClass} border-danger`);
+
+h1.classList.remove('border-danger');
+h1.classList.add('test');
