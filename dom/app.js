7a39920365efe1d03ba66cc37690c96ac3bd17b0
dannyhma <dannyhimawan089@gmail.com>
2024-01-02 22:45:06 +0700

    append, createelement, & insertadjacent

--- dom/app.js
+++ dom/app.js
@@ -1,14 +1,24 @@
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
 // for (let heading of headings) {
 //   heading.style.color = "blue";
 // }
 
-h1.getAttribute('class');
-h1.setAttribute('class', 'bg-info');
+// h1.getAttribute('class');
+// h1.setAttribute('class', 'bg-info');
 
-let currentClass = h1.getAttribute('class');
-h1.setAttribute('class', `${currentClass} border-danger`);
+// let currentClass = h1.getAttribute('class');
+// h1.setAttribute('class', `${currentClass} border-danger`);
 
-h1.classList.remove('border-danger');
-h1.classList.add('test');
+// h1.classList.remove('border-danger');
+// h1.classList.add('test');
+
+const p = document.querySelector('p');
+// p.innerText = 'lorem lorem lorem';
+p.append('<b>test</b>');
+
+const h2 = document.createElement('h2');
+h2.append('Test createelement dan afterend');
+
+const h1 = document.querySelector('h1');
+h1.insertAdjacentElement('afterend', h2);
