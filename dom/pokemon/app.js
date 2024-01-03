65635cf717fec039f4b1d95376226ba983c2e58d
dannyhma <dannyhimawan089@gmail.com>
2024-01-03 09:48:17 +0700

    styling pokemon

--- dom/pokemon/app.js
+++ dom/pokemon/app.js
@@ -1,18 +1,18 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.getElementById('container'); // ambil element berdasarkan id
+const container = document.getElementById("container"); // ambil element berdasarkan id
 const baseImgURL =
-  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'; // ambil image
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; // ambil image
 
 // lakukan perulangan
-for (let i = 1; i <= 500; i++) {
-  const pokeBall = document.createElement('div'); // buat element div simpan ke pokeBall
-  pokeBall.classList.add('pokemon'); // buat class untuk div
+for (let i = 1; i <= 1000; i++) {
+  const pokeBall = document.createElement("div"); // buat element div simpan ke pokeBall
+  pokeBall.classList.add("pokemon"); // buat class untuk div
 
-  const imgPokemon = document.createElement('img'); // buat element img simpan ke imgPokemon
+  const imgPokemon = document.createElement("img"); // buat element img simpan ke imgPokemon
   imgPokemon.src = `${baseImgURL}${i}.png`; // simpan source image ke imgPokemon
 
-  const label = document.createElement('span'); // buat element label simpan ke label
+  const label = document.createElement("span"); // buat element label simpan ke label
   label.innerText = `#${i}`; // simpan perulangan ke label
 
   pokeBall.appendChild(imgPokemon); // tampilkan image di div
