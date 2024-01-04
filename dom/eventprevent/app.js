73a363449dc680780b6517c09f1dec110d3eb678
dannyhma <dannyhimawan089@gmail.com>
2024-01-04 12:30:21 +0700

    update code

--- dom/eventprevent/app.js
+++ dom/eventprevent/app.js
@@ -2,7 +2,16 @@ const form = document.getElementById('form');
 const input = document.querySelector('input');
 const list = document.getElementById('notes');
 
-form.addEventListener('submit', function (e) {
+// input.addEventListener('change', (e) => {
+//   console.log('Nilai Berubah');
+// });
+
+input.addEventListener('input', (e) => {
+  document.querySelector('h3').innerText = input.value;
+  console.log('Nilai Berhasil Diinput');
+});
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   const noteValue = input.value;
   const newList = document.createElement('li');
