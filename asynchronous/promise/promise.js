72fd212bd2562bb53cc38b7a80ab65313184a2f5
dannyhma <dannyhimawan089@gmail.com>
2024-01-07 21:53:28 +0700

    promise

---
+++ asynchronous/promise/promise.js
@@ -0,0 +1,34 @@
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject('Error: Connection Timeout');
+      } else {
+        resolve(`Success: ${url} (${delay}ms)`);
+      }
+    });
+  });
+};
+
+requestPromise('movie.com')
+  .then((response) => {
+    console.log('Success', response);
+    requestPromise('movie.com')
+      .then((response) => {
+        console.log('Success', response);
+        requestPromise('movie.com')
+          .then((response) => {
+            console.log('Success', response);
+          })
+          .catch((error) => {
+            console.log('Error', error);
+          });
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  })
+  .catch((error) => {
+    console.log('Error', error);
+  });
