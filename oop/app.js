710ee118f835e26a921e744dd43945f235c92564
dannyhma <dannyhimawan089@gmail.com>
2024-01-13 12:39:58 +0700

    membuat object dengan class

--- oop/app.js
+++ oop/app.js
@@ -26,23 +26,57 @@ function convertColor(r, g, b) {
   return color;
 }
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = g;
-  this.b = b;
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+    console.log('Hi, ini adalah proses constructor');
+  }
+
+  colorName() {
+    console.log(`The color name is ${this.name}`);
+  }
+
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+
+  hex() {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
 }
 
-Color.prototype.rgb = function () {
-  const { r, g, b } = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
-
-Color.prototype.hex = function () {
-  const { r, g, b } = this;
-  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-
-Color.prototype.rgba = function (a = 1.0) {
-  const { r, g, b } = this;
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-};
+// new Color();
